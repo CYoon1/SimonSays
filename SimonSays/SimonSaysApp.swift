ee7a75c067138c8a334e229b0872a33db6556d5e
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2021-10-10 21:57:24 -0400

    Initial Commit

---
+++ SimonSays/SimonSaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonSaysApp.swift
+//  SimonSays
+//
+//  Created by Christopher Yoon on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SimonSaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
